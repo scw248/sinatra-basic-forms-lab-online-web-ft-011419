498ed0820b51aab040879144ca0f5d15533dc74f
Stephen Wright <scw248@gmail.com>
2019-02-26 21:50:41 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,7 +2,7 @@ class Puppy
   
   attr_accessor :name, :breed, :age
   
-  def initialize(name: nil, breed: nil, age: nil)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
